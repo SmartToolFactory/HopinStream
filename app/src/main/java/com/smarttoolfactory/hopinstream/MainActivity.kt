992639ee65f7f2c65825e600d0fff852558145ce
Smart Tool Factory <smarttoolfactory@icloud.com>
2021-08-14 17:10:30 +0300

    Setup di for core and app modules

--- app/src/main/java/com/smarttoolfactory/hopinstream/MainActivity.kt
+++ app/src/main/java/com/smarttoolfactory/hopinstream/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.smarttoolfactory.hopinstream
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -8,11 +9,30 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.smarttoolfactory.domain.usecase.LoginUseCase
+import com.smarttoolfactory.domain.usecase.StagesUseCase
 import com.smarttoolfactory.hopinstream.ui.theme.HopinStreamTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var loginUseCase: LoginUseCase
+
+    @Inject
+    lateinit var stagesUseCase: StagesUseCase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Toast.makeText(
+            applicationContext,
+            "LoginUseCase: $loginUseCase, stagesUseCase: $stagesUseCase",
+            Toast.LENGTH_SHORT
+        ).show()
+
         setContent {
             HopinStreamTheme {
                 // A surface container using the 'background' color from the theme
---
+++ libraries/core/src/main/java/com/smarttoolfactory/core/di/CoreModuleDependencies.kt
@@ -0,0 +1,20 @@
+package com.smarttoolfactory.core.di
+
+import com.smarttoolfactory.domain.usecase.LoginUseCase
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * This component is required for adding dependencies to Dynamic Feature Modules by
+ * adding [CoreModule] as dependent component
+ */
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface CoreModuleDependencies {
+
+    /*
+     Provision methods to provide dependencies to components that depend on this component
+   */
+    fun loginUsesCase(): LoginUseCase
+}
